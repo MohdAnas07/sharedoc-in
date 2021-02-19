14c77b792f90670e69f73b90bf90eae0c09c5cfe
Mohd Anas <anas98144@gmail.com>
2021-02-19 00:34:57 -0800

    fix bug

--- routes/files.js
+++ routes/files.js
@@ -30,7 +30,7 @@ router.post('/', (req, res) => {
             size: req.file.size
         });
         const response = await file.save();
-        res.json({ file: `${process.env.APP_BASE_URL}files/${response.uuid}` });
+        res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` });
     });
 });
 
